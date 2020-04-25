18c9c3a81cef3c9aecdbe8ea69739dcd741f3464
TheGuardianWolf <nano@pixelcollider.net>
2020-04-25 20:28:28 +1200

    Hover fix

--- src/component/GalleryImage/GalleryImage.jsx
+++ src/component/GalleryImage/GalleryImage.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import { createStyles, withStyles } from '@material-ui/core/styles';
+
 import Paper from '@material-ui/core/Paper';
+import React from 'react';
+import placeholder from '../../assets/logo512.png';
 import zIndex from '@material-ui/core/styles/zIndex';
-const placeHolder = require('../../assets/logo512.png');
 
 const theme = {
   spacing: 8,
@@ -13,15 +14,20 @@ const elevation = 0;
 let styles = () => createStyles ({
   paper: {
     textAlign: 'center',
+    position: 'relative'
   },
   image: {
     maxWidth: '100%',
   },
   hover: {
+    top: 0,
+    left: 0,
     height: '100%',
-    maxWidth: '100%',
+    width: '100%',
     opacity: 0.5,
-    backgroundColor: 'black', 
+    backgroundColor: 'black',
+    zIndex: 1,
+    position: 'absolute'
   }
 })
 
@@ -30,14 +36,16 @@ class GalleryImage extends React.Component {
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
-    this.state = {hover: true};
+    this.state = {hover: false};
   }
 
   onHover = () => {
+    console.log('hovered');
     this.setState({hover: true})
   }
 
   onLeave = () => {
+    console.log('left');
     this.setState({hover: false})
   }
 
@@ -53,14 +61,12 @@ class GalleryImage extends React.Component {
     }
 
     return <div className="gallery-image" onMouseEnter={this.onHover} onMouseLeave={this.onLeave}>
-          <Paper className={this.props.classes.paper} elevation={elevation}>
-            <img className={this.props.classes.image} src={placeHolder} alt="placeholder" onMouseEnter={this.onHover} onMouseLeave={this.onLeave}></img>
-            <div className={this.props.classes.hover}></div>
+          <Paper className={this.props.classes.paper} elevation={elevation} onMouseEnter={this.onHover} onMouseLeave={this.onLeave}>
+            <img className={this.props.classes.image} src={placeholder} alt="placeholder"  ></img>
+            {this.state.hover && <div className={this.props.classes.hover}></div>}
           </Paper>
     </div>
   }
 }
 
 export default withStyles(styles)(GalleryImage);
-
-
